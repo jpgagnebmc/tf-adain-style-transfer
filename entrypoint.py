d633bf79401c79f4b83569697ecf9003a64baf54
Kevin Baragona <kmbaragona@gmail.com>
2019-03-15 12:26:51 -0700

    rename integration library - add inputs schema

--- entrypoint.py
+++ entrypoint.py
@@ -1,7 +1,14 @@
-from models_integration_library.public_interface import start_loop
+from ai_integration.public_interface import start_loop
 
 from inference import initialize_model, infer
 
 initialize_model()
 
-start_loop(infer)
+start_loop(inference_function=infer, inputs_schema={
+    "style": {
+        "type": "image"
+    },
+    "content": {
+        "type": "image"
+    },
+})

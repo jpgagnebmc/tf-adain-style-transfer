fa22049c611ec8ee96472f875cd8912575410b07
Kevin Baragona <kmbaragona@gmail.com>
2019-04-05 15:34:50 -0700

    simplify entrypoint

--- entrypoint.py
+++ entrypoint.py
@@ -1,14 +1,3 @@
-from ai_integration.public_interface import start_loop
-
-from inference import initialize_model, infer
+from inference import initialize_model
 
 initialize_model()
-
-start_loop(inference_function=infer, inputs_schema={
-    "style": {
-        "type": "image"
-    },
-    "content": {
-        "type": "image"
-    },
-})
